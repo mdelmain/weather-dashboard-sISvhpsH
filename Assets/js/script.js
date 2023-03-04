eab5f8fcaaec64866a2f6183cb74f3133a2cf0dd
Mandy Delmaine <mandydelmaine@gmail.com>
2023-03-03 19:17:09 -0500

    added form to search for city

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,15 @@
-var APIKey = 9083443274c27542ddc7e8c2a820d282
+var APIKey = "9083443274c27542ddc7e8c2a820d282";
+var countryCode = "840";
+
+$(function () {
+  formSubmitHandler();
+});
+
+function formSubmitHandler() {
+  $(".btn").click(function (event) {
+    event.preventDefault();
+
+    var cityName = $("#city-name").get(0).value;
+    console.log(cityName);
+  });
+}
