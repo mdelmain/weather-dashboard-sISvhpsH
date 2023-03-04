3c7c8df7726cd4dc3ca21f4c86418c50e0c16d84
Mandy Delmaine <mandydelmaine@gmail.com>
2023-03-03 21:08:50 -0500

    created function to get city coordinates and created function to get current weather

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,6 +10,50 @@ function formSubmitHandler() {
     event.preventDefault();
 
     var cityName = $("#city-name").get(0).value;
-    console.log(cityName);
+    getCoordinates(cityName);
   });
+}
+
+function getCoordinates(cityName) {
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "," + countryCode +"&limit=1&appid=" + APIKey;
+    console.log(apiUrl);
+
+    fetch (apiUrl)
+    .then(function (response) {
+        return response.json();
+      })
+    .then(function (data) {
+        var lat = data[0].lat;
+        var lon = data[0].lon;
+        console.log(lat);
+        console.log(lon);
+        getCurrentWeather(lat, lon);
+    });
+}
+
+function getCurrentWeather(lat, lon) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+    console.log("getcurrent", apiUrl);
+   
+    fetch (apiUrl)
+    .then(function (response) {
+        return response.json();
+      })
+    .then(function (data) {
+        console.log(data);
+        var temp  = data.main.temp;
+        var wind = data.wind.speed;
+        var humidity = data.main.humidity;
+        var iconcode = data.weather[0].icon;
+        var icon = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        console.log(icon);
+        console.log(temp);
+        console.log(wind);
+        console.log(humidity);
+    });
+
+}
+
+function updateCurrentWeatherContainer () {
+
 }
