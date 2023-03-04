798a516e7a3f27adcc5ca92e6b367d12282916df
Mandy Delmaine <mandydelmaine@gmail.com>
2023-03-04 13:38:34 -0500

    updated card container content and styling

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -30,6 +30,7 @@ function getCoordinates(cityName) {
         console.log(lat);
         console.log(lon);
         getCurrentWeather(lat, lon);
+        getFiveDayForecast(lat, lon);
         var today = dayjs().format("MM/DD/YY")
         $("#current-weather-city").text(data[0].name + ", " + data[0].state + " (" + today +  ")");
     });
@@ -64,4 +65,53 @@ function updateCurrentWeatherContainer (icon, temp, wind, humidity) {
     $("#temp").text("Temp: " + temp + "°F");
     $("#wind").text("Wind: " + wind + " MPH");
     $("#humidity").text("Humidity: " + humidity + " %");
+}
+
+function getFiveDayForecast(lat, lon) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+
+    fetch (apiUrl)
+    .then(function (response) {
+        return response.json();
+      })
+    .then(function (data) {
+        console.log("test")
+        console.log({label:"test", data});
+        console.log("test", data.list[3].dt);
+        //var date =  dayjs.unix(data.list[3].dt).format("MM/DD/YY")
+        //console.log("date", date);
+        
+        //console.log("today", currentDateObject);
+        for (var i = 1; i <=5;  i++) {
+            var dayForecast = getDayForecast(i, data);
+            var date = dayjs.unix(dayForecast.dt).format("MM/DD/YY");
+            var temp  = dayForecast.main.temp;
+            var wind = dayForecast.wind.speed;
+            var humidity = dayForecast.main.humidity;
+            var iconcode = dayForecast.weather[0].icon;
+            var icon = "http://openweathermap.org/img/w/" + iconcode + ".png";
+            updateForecastWeatherContainer (i, date, icon, temp, wind, humidity);
+        }
+   
+       
+    });
+}
+
+function getDayForecast(days, data) {
+    var targetDateObject = dayjs().utc().hour(12).minute(0).second(0).millisecond(0).add(days, "day");
+    for (var i = 0; i < data.list.length; i++){
+        var forecastDateObject =  dayjs.unix(data.list[i].dt);
+        //console.log("forecastdate",  forecastDateObject);
+        if (forecastDateObject >= targetDateObject){
+            return data.list[i];
+        }
+    }
+}
+
+function updateForecastWeatherContainer (id, date, icon, temp, wind, humidity) {
+    $("#day"+id+ " .day").text(date);
+    $("#day"+id+ " .icon").attr("src", icon);
+    $("#day"+id+ " .temp").text("Temp: " + temp + "°F");
+    $("#day"+id+ " .wind").text("Wind: " + wind + " MPH");
+    $("#day"+id+ " .humidity").text("Humidity: " + humidity + " %");
 }
